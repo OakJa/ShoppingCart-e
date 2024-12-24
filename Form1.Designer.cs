8c1ee38e564f5c2e59c519ca1a07c8f6ce4502f2
OakJa <phutthiphong.n@kkumail.com>
2024-12-24 11:39:16 +0700

    update

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -37,6 +37,27 @@
             label1 = new Label();
             tbMusshaQuantity = new TextBox();
             tbCoffeeQunatity = new TextBox();
+            label2 = new Label();
+            tbCast = new TextBox();
+            label3 = new Label();
+            textBox1000 = new TextBox();
+            label4 = new Label();
+            textBox500 = new TextBox();
+            label5 = new Label();
+            textBox20 = new TextBox();
+            label6 = new Label();
+            textBox50 = new TextBox();
+            label7 = new Label();
+            textBox100 = new TextBox();
+            label8 = new Label();
+            textBox_1 = new TextBox();
+            label9 = new Label();
+            textBox5 = new TextBox();
+            label10 = new Label();
+            textBox10 = new TextBox();
+            label11 = new Label();
+            tbprice_to_pay = new TextBox();
+            label12 = new Label();
             SuspendLayout();
             // 
             // tbCoffeeprice
@@ -88,7 +109,7 @@
             // 
             // total
             // 
-            total.Location = new Point(235, 128);
+            total.Location = new Point(578, 41);
             total.Name = "total";
             total.Size = new Size(100, 23);
             total.TabIndex = 6;
@@ -96,7 +117,7 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(188, 131);
+            label1.Location = new Point(531, 44);
             label1.Name = "label1";
             label1.Size = new Size(31, 15);
             label1.TabIndex = 7;
@@ -119,11 +140,202 @@
             tbCoffeeQunatity.TabIndex = 8;
             tbCoffeeQunatity.TextChanged += tbCoffeeQunatity_TextChanged;
             // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(531, 73);
+            label2.Name = "label2";
+            label2.Size = new Size(30, 15);
+            label2.TabIndex = 11;
+            label2.Text = "Cast";
+            // 
+            // tbCast
+            // 
+            tbCast.Location = new Point(578, 70);
+            tbCast.Name = "tbCast";
+            tbCast.Size = new Size(100, 23);
+            tbCast.TabIndex = 10;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(531, 132);
+            label3.Name = "label3";
+            label3.Size = new Size(31, 15);
+            label3.TabIndex = 13;
+            label3.Text = "1000";
+            label3.Click += label3_Click;
+            // 
+            // textBox1000
+            // 
+            textBox1000.Location = new Point(578, 129);
+            textBox1000.Name = "textBox1000";
+            textBox1000.Size = new Size(100, 23);
+            textBox1000.TabIndex = 12;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(531, 165);
+            label4.Name = "label4";
+            label4.Size = new Size(25, 15);
+            label4.TabIndex = 15;
+            label4.Text = "500";
+            // 
+            // textBox500
+            // 
+            textBox500.Location = new Point(578, 162);
+            textBox500.Name = "textBox500";
+            textBox500.Size = new Size(100, 23);
+            textBox500.TabIndex = 14;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(531, 257);
+            label5.Name = "label5";
+            label5.Size = new Size(19, 15);
+            label5.TabIndex = 21;
+            label5.Text = "20";
+            // 
+            // textBox20
+            // 
+            textBox20.Location = new Point(578, 254);
+            textBox20.Name = "textBox20";
+            textBox20.Size = new Size(100, 23);
+            textBox20.TabIndex = 20;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(531, 224);
+            label6.Name = "label6";
+            label6.Size = new Size(19, 15);
+            label6.TabIndex = 19;
+            label6.Text = "50";
+            // 
+            // textBox50
+            // 
+            textBox50.Location = new Point(578, 221);
+            textBox50.Name = "textBox50";
+            textBox50.Size = new Size(100, 23);
+            textBox50.TabIndex = 18;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(531, 195);
+            label7.Name = "label7";
+            label7.Size = new Size(25, 15);
+            label7.TabIndex = 17;
+            label7.Text = "100";
+            // 
+            // textBox100
+            // 
+            textBox100.Location = new Point(578, 192);
+            textBox100.Name = "textBox100";
+            textBox100.Size = new Size(100, 23);
+            textBox100.TabIndex = 16;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(531, 353);
+            label8.Name = "label8";
+            label8.Size = new Size(13, 15);
+            label8.TabIndex = 27;
+            label8.Text = "1";
+            // 
+            // textBox_1
+            // 
+            textBox_1.Location = new Point(578, 350);
+            textBox_1.Name = "textBox_1";
+            textBox_1.Size = new Size(100, 23);
+            textBox_1.TabIndex = 26;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(531, 320);
+            label9.Name = "label9";
+            label9.Size = new Size(13, 15);
+            label9.TabIndex = 25;
+            label9.Text = "5";
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(578, 317);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(100, 23);
+            textBox5.TabIndex = 24;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(531, 291);
+            label10.Name = "label10";
+            label10.Size = new Size(19, 15);
+            label10.TabIndex = 23;
+            label10.Text = "10";
+            // 
+            // textBox10
+            // 
+            textBox10.Location = new Point(578, 288);
+            textBox10.Name = "textBox10";
+            textBox10.Size = new Size(100, 23);
+            textBox10.TabIndex = 22;
+            // 
+            // label11
+            // 
+            label11.Location = new Point(0, 0);
+            label11.Name = "label11";
+            label11.Size = new Size(100, 23);
+            label11.TabIndex = 0;
+            // 
+            // tbprice_to_pay
+            // 
+            tbprice_to_pay.Location = new Point(578, 100);
+            tbprice_to_pay.Name = "tbprice_to_pay";
+            tbprice_to_pay.Size = new Size(100, 23);
+            tbprice_to_pay.TabIndex = 28;
+            tbprice_to_pay.TextChanged += textBox1_TextChanged;
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Location = new Point(503, 103);
+            label12.Name = "label12";
+            label12.Size = new Size(69, 15);
+            label12.TabIndex = 29;
+            label12.Text = "price to pay";
+            label12.Click += label12_Click;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(480, 207);
+            ClientSize = new Size(748, 408);
+            Controls.Add(label12);
+            Controls.Add(label11);
+            Controls.Add(tbprice_to_pay);
+            Controls.Add(label8);
+            Controls.Add(textBox_1);
+            Controls.Add(label9);
+            Controls.Add(textBox5);
+            Controls.Add(label10);
+            Controls.Add(textBox10);
+            Controls.Add(label5);
+            Controls.Add(textBox20);
+            Controls.Add(label6);
+            Controls.Add(textBox50);
+            Controls.Add(label7);
+            Controls.Add(textBox100);
+            Controls.Add(label4);
+            Controls.Add(textBox500);
+            Controls.Add(label3);
+            Controls.Add(textBox1000);
+            Controls.Add(label2);
+            Controls.Add(tbCast);
             Controls.Add(tbMusshaQuantity);
             Controls.Add(tbCoffeeQunatity);
             Controls.Add(label1);
@@ -151,5 +363,26 @@
         private Label label1;
         private TextBox tbMusshaQuantity;
         private TextBox tbCoffeeQunatity;
+        private Label label2;
+        private TextBox tbCast;
+        private Label label3;
+        private TextBox textBox1000;
+        private Label label4;
+        private TextBox textBox500;
+        private Label label5;
+        private TextBox textBox20;
+        private Label label6;
+        private TextBox textBox50;
+        private Label label7;
+        private TextBox textBox100;
+        private Label label8;
+        private TextBox textBox_1;
+        private Label label9;
+        private TextBox textBox5;
+        private Label label10;
+        private TextBox textBox10;
+        private Label label11;
+        private TextBox tbprice_to_pay;
+        private Label label12;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -13,14 +13,18 @@ namespace ShoppingCart_e
             string strCoffeeQunatity = tbCoffeeQunatity.Text;
             string strMusshaprice = tbMusshaprice.Text;
             string strMusshaQuantity = tbMusshaQuantity.Text;
+            string strCast = tbCast.Text;
 
             int iCoffeeprice = int.Parse(strCoffeeprice);
             int iCoffeeQuanitity = int.Parse(strCoffeeQunatity);  // Corrected line
             int iMusshaprice = int.Parse(strMusshaprice);
             int iMusshaQuantity = int.Parse(strMusshaQuantity);
+            int iCast = int.Parse(strCast);
 
             double isum1 = iCoffeeprice * iCoffeeQuanitity;
             double isum2 = iMusshaprice * iMusshaQuantity;
+            double isum3 = iCast;
+
 
             double totalSum = 0;
 
@@ -35,12 +39,14 @@ namespace ShoppingCart_e
             }
 
             total.Text = totalSum.ToString();
+            tbCast.Text = isum3.ToString();
+            tbprice_to_pay.Text = (isum1 + isum2).ToString();
         }
 
 
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
-            
+
 
         }
 
@@ -57,7 +63,7 @@ namespace ShoppingCart_e
         private void tbCoffeeprice_TextChanged(object sender, EventArgs e)
         {
             {
-                
+
 
 
             }
@@ -65,17 +71,32 @@ namespace ShoppingCart_e
 
         private void tbMusshaprice_TextChanged(object sender, EventArgs e)
         {
-           
+
         }
 
         private void tbCoffeeQunatity_TextChanged(object sender, EventArgs e)
         {
-            
+
         }
 
         private void tbMusshaQuantity_TextChanged(object sender, EventArgs e)
         {
-         
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label12_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
